4eae308a84fe0b40ec20bcaebf27e3246d6622dd
Pera030 <pericapaunovic@gmail.com>
2021-05-21 23:29:57 +0200

    prva promena

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timezone</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/gif" href="img/favicon.ico">
+</head>
+<body>
+    
+    <!-- Navigation -->
+    <nav id="nav">
+        <a href="index.php" class="logo"><img src="img/logo.png" alt=""></a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="lastest.php">Latest</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="pages.php">Pages</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><p>HOT</p></li>
+        </ul>
+        <div class="icons">
+            <a href=""><i class="fas fa-search"></i></a>
+            <a href=""><i class="far fa-user"></i></a>
+            <a href=""><i class="fas fa-shopping-cart"></i></a>
+        </div>
+    </nav>
+    
+    <!-- Header-->
+    <header>
+        <div class="container">
+            <div class="col">
+                <h1>Select Your New <br> Perfect Style</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex beatae atque quis aspernatur, provident<br> debitis porro harum. Exercitationem, expedita, eius?</p>
+                <button type="button">Shop now</button>
+            </div>
+            <div class="col">
+                <img src="img/watch.png" alt="">
+            </div>
+        </div>
+    </header>
+    
+    <!-- New Arrivals -->
+    <section class="container newArrivals">
+        <h2>New Arrivals</h2>
+        <div class="row">
+            <div class="col">
+                <img src="img/new_product1.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/new_product2.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/new_product3.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Watch -->
+    <section class="watch">
+        <article>
+            <img src="img/gallery1.png" alt="">
+        </article>
+        <article>
+            <img src="img/gallery2.png" alt="">
+        </article>
+        <article>
+            <div class="row">
+                <img src="img/gallery3.png" alt="">
+            </div>
+            <div class="row">
+                <img src="img/gallery4.png" alt="">
+            </div>
+        </article>
+    </section>
+    
+    <!-- Popular -->
+    <section class="popular container">
+        <h2>Popular items</h2>
+        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna<br> aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+        <div class="row">
+            <div class="col">
+                <img src="img/popular1.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/popular2.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/popular3.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/popular4.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/popular5.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <div class="col">
+                <img src="img/popular6.png" alt="">
+                <h4><a href="">Thermo Ball Etip Gloves</a></h4>
+                <p>$ 45,743</p>
+            </div>
+            <button type="button">View more products</button>
+        </div>
+    </section>
+    
+    <!-- Choice -->
+    <section class="choice container">
+        <div class="row">
+            <div class="col">
+                <h2>Watch of Choice</h2>
+                <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                <button type="button">Show watches</button>
+            </div>
+            <div class="col">
+                <img src="img/choce_watch1.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <img src="img/choce_watch2.png" alt="">
+            </div>
+            <div class="col">
+                <h2>Watch of Choice</h2>
+                <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                <button type="button">Show watches</button>
+            </div>
+        </div>
+    </section>
+    
+    <!-- Secure -->
+    <section class="secure container">
+        <div class="col">
+            <i class="fas fa-cube"></i>
+            <h4>Free Shipping Method</h4>
+            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-unlock-alt"></i>
+            <h4>Secure Payment System</h4>
+            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-sync"></i>
+            <h4>Secure Payment System</h4>
+            <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+        </div>
+    </section>
+    
+    <!-- Footer -->
+    <footer class="container">
+        <div class="col">
+            <img src="img/logo2_footer.png" alt="">
+            <p>Asorem ipsum adipolor sdit amet, consectetur adipisicing elitcf sed do eiusmod tem.</p>
+        </div>
+        <div class="col">
+            <h4>Quick Links</h4>
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Offer &amp; discouts</a></li>
+                <li><a href="">Get Coupon</a></li>
+                <li><a href="">Contact us</a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <h4>New products</h4>
+            <ul>
+                <li><a href="#nav">Woman clothes</a></li>
+                <li><a href="#nav">Fashion Accessories</a></li>
+                <li><a href="#nav">Man Accessories</a></li>
+                <li><a href="#nav">Rubber made toys</a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <h4>Support</h4>
+            <ul>
+                <li><a href="#nav">Frequently Asked Questions</a></li>
+                <li><a href="#nav">Terms &amp; Conditions</a></li>
+                <li><a href="#nav">Privacy Policy</a></li>
+                <li><a href="#nav">Report a Payment Issue</a></li>
+            </ul>
+        </div>
+    </footer>
+    
+    <!-- Bottom bar-->
+    <section class="bottomBar container">
+        <div class="col">
+            <p>Copyright &copy;2020 All rights reserved | This template is made with <i class="fas fa-heart"></i> by <a href="">Colorlib</a></p>
+        </div>
+        <div class="col">
+            <a href=""><i class="fab fa-twitter"></i></a>
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+            <a href=""><i class="fab fa-behance"></i></a>
+            <a href=""><i class="fas fa-globe"></i></a>
+        </div>
+    </section>
+    
+    
+    
+    
+</body>
+</html>
